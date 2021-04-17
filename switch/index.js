398839bab3e9006dae30e1e57314f4cae5fe56c7
Marco Barreto <kinhobarreto09@gmail.com>
2021-04-17 12:08:26 -0300

    Estrutura Switch

---
+++ switch/index.js
@@ -0,0 +1,34 @@
+function getDayAsText(){
+    let diaSemanaTexto;
+    switch(diaDaSemana){
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terca';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sabado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = '';
+            return diaSemanaTexto;
+    }
+    
+}
+const data = new Date;
+const diaDaSemana = data.getDay();
+const diaSemanaTexto;
+diaSemanaTexto = getDayAsText(diaDaSemana);
